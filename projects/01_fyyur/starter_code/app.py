0ff8db3791e949df272fd9f08691999d9ae817de
Montikor <60382371+Montikor@users.noreply.github.com>
2020-03-23 17:22:16 +0100

    first overview
    installed venv
    getting familiar with the stuff
    small changes on forms and app
    connected to database

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -6,6 +6,7 @@ import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 import logging
@@ -20,8 +21,7 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
-
-# TODO: connect to a local postgresql database
+migrate = Migrate(app,db)
 
 #----------------------------------------------------------------------------#
 # Models.
@@ -36,8 +36,13 @@ class Venue(db.Model):
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    genres = db.Column(db.String(120)) 
+    image_link = db.Column(db.String(500)) 
     facebook_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean())  # not in post html yet
+    seeking_description = db.Column(db.String(500))  # not in post html yet
+    # upcoming_shows
+    # past_shows
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
@@ -50,10 +55,18 @@ class Artist(db.Model):
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    image_link = db.Column(db.String(500))  ## not yet in post html?
     facebook_link = db.Column(db.String(120))
+    # upcoming shows
+    # past shows
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+class Show(db.Model):
+    __tablename__= 'Show'
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime)
+    # artist_id
+    # venue_id
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
